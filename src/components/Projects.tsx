d1d62d9f2b846f7d60ccabf92bcbe4b2343b59ab
Alexander Ho <ho.alexander.g@gmail.com>
2023-07-19 21:49:07 +0800

    refactor: separate card element form projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,7 @@
+import ProjectCard from './layouts/ProjectCard';
 import styles from '@styles/modules/Projects.module.css';
 import blogPostPreview from '@assets/previews/blog-post.png';
 import stickyThoughtsPreview from '@assets/previews/sticky-thoughts.png';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Projects = () => {
   return (
@@ -12,100 +9,34 @@ const Projects = () => {
       <div className={`${styles.projects__container} container container--section`}>
         <h2>Projects</h2>
 
-        <article className={styles.card}>
-          <img
-            src={stickyThoughtsPreview}
-            alt="sticky-thoughts-preview"
-            className={styles.card__img}
-          />
-
-          <div className={styles.card__info}>
-            <h3 className={styles.card__title}>Sticky Thoughts</h3>
-            <p className={styles.card__desc}>
+        <ProjectCard
+          title="Sticky Thoughts"
+          description={
+            <>
               Post your thoughts, opinions, and experiences anonymously. Whether you&apos;re looking
               to vent about your day, share your political views, or connect with other people.
-            </p>
+            </>
+          }
+          image={stickyThoughtsPreview}
+          techStack={['React', 'Firebase', 'Mantine']}
+          liveLink="https://sticky-thoughts.netlify.app/"
+          sourceLink="https://github.com/alexndrho/sticky-thoughts"
+        />
 
-            <div className={styles['card__tech-stacks']}>
-              <span className={styles['card__tech-stack']}>React</span>
-              <span className={styles['card__tech-stack']}>Firebase</span>
-              <span className={styles['card__tech-stack']}>Mantine</span>
-            </div>
-
-            <div className={styles.card__links}>
-              <a
-                href="https://sticky-thoughts.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.card__link}
-              >
-                Live
-                <span className={styles.card__link__icon}>
-                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                </span>
-              </a>
-
-              <a
-                href="https://github.com/alexndrho/sticky-thoughts"
-                target="_blank"
-                rel="noreferrer"
-                className={`${styles.card__link}`}
-              >
-                Source
-                <span className={styles.card__link__icon}>
-                  <FontAwesomeIcon icon={faGithub} />
-                </span>
-              </a>
-            </div>
-          </div>
-        </article>
-
-        <article className={styles.card}>
-          <img src={blogPostPreview} alt="blog-post-preview" className={styles.card__img} />
-
-          <div className={styles.card__info}>
-            <h3 className={styles.card__title}>Blog</h3>
-            <p className={styles.card__desc}>
+        <ProjectCard
+          title="Blog"
+          description={
+            <>
               A blog posting website allows users to create an account, write blogs in Markdown
               format, and add photos, videos, and other media. Users can also update their personal
               information and upload a profile picture.
-            </p>
-
-            <div className={styles['card__tech-stacks']}>
-              <span className={styles['card__tech-stack']}>MongoDB</span>
-              <span className={styles['card__tech-stack']}>Express</span>
-              <span className={styles['card__tech-stack']}>React</span>
-              <span className={styles['card__tech-stack']}>Node</span>
-              <span className={styles['card__tech-stack']}>Stitches</span>
-            </div>
-
-            <div className={styles.card__links}>
-              <a
-                href="https://blog-post-k6bm.onrender.com/"
-                target="_blank"
-                rel="noreferrer"
-                className={`${styles.card__link}`}
-              >
-                Live
-                <span className={styles.card__link__icon}>
-                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                </span>
-              </a>
-
-              <a
-                href="https://github.com/alexndrho/blog-post"
-                target="_blank"
-                rel="noreferrer"
-                className={`${styles.card__link}`}
-              >
-                Source
-                <span className={styles.card__link__icon}>
-                  <FontAwesomeIcon icon={faGithub} />
-                </span>
-              </a>
-            </div>
-          </div>
-        </article>
+            </>
+          }
+          image={blogPostPreview}
+          techStack={['MongoDB', 'Express', 'React', 'Node', 'Stitches']}
+          liveLink="https://blog-post-k6bm.onrender.com/"
+          sourceLink="https://github.com/alexndrho/blog-post"
+        />
       </div>
     </section>
   );
---
+++ src/components/layouts/ProjectCard.tsx
@@ -0,0 +1,69 @@
+import styles from '@styles/modules/layouts/ProjectCard.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from 'react';
+
+interface ProjectCardProps {
+  title: React.ReactNode;
+  description: React.ReactNode;
+  image: string;
+  techStack: string[];
+  liveLink?: string;
+  sourceLink?: string;
+}
+
+const ProjectCard = ({
+  title,
+  description,
+  image,
+  techStack,
+  liveLink,
+  sourceLink,
+}: ProjectCardProps) => {
+  return (
+    <article className={styles.card}>
+      <img src={image} alt="project-preview" className={styles.card__img} />
+
+      <div className={styles.card__info}>
+        <h3 className={styles.card__title}>{title}</h3>
+        <p className={styles.card__desc}>{description}</p>
+
+        <div className={styles['card__tech-stacks']}>
+          {techStack.map((tech) => (
+            <span key={tech} className={styles['card__tech-stack']}>
+              {tech}
+            </span>
+          ))}
+        </div>
+
+        <div className={styles.card__links}>
+          {liveLink && (
+            <a href={liveLink} target="_blank" rel="noreferrer" className={styles.card__link}>
+              Live
+              <span className={styles.card__link__icon}>
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              </span>
+            </a>
+          )}
+
+          {sourceLink && (
+            <a
+              href={sourceLink}
+              target="_blank"
+              rel="noreferrer"
+              className={`${styles.card__link}`}
+            >
+              Source
+              <span className={styles.card__link__icon}>
+                <FontAwesomeIcon icon={faGithub} />
+              </span>
+            </a>
+          )}
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default ProjectCard;
