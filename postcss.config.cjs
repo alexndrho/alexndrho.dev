d6f79c547d3aa85a66971b0e9c8c4366f894630b
alex <80305180+alexndrho@users.noreply.github.com>
2023-08-30 06:59:22 +0000

    feat(postcss): add custom media queries

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,10 @@
 module.exports = {
-  plugins: [require('autoprefixer'), require('cssnano')],
+  plugins: [
+    require('autoprefixer'),
+    require('@csstools/postcss-global-data')({
+      files: ['./src/styles/custom-media.css'],
+    }),
+    require('postcss-custom-media'),
+    require('cssnano'),
+  ],
 };
