07ab0f71a11684129392e365a11a65e298f29518
Alexander Gabriel Ho <80305180+alexndrho@users.noreply.github.com>
2024-07-08 12:04:05 +0800

    feat: add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from "./markdown-utils.mjs";
-
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
