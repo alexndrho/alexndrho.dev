2c17979eb932620153be6b1b2d372d08c428b7ac
Alexander Gabriel Ho <80305180+alexndrho@users.noreply.github.com>
2024-07-08 10:01:35 +0800

    feat: add minute read time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import { remarkReadingTime } from "./markdown-utils.mjs";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   vite: {
     ssr: {
       noExternal: ["react-icons"],
