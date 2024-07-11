13247c5ea6c5309bc8f9a9b50fed5a586939255c
Alexander Gabriel Ho <80305180+alexndrho@users.noreply.github.com>
2024-07-11 16:15:23 +0800

    Feature/blog (#10)
    * fix: misuse of z-index
    
    * feat: add blog post
    
    * feat: add blog index page
    
    * fix: separate scroll to top from blog layout
    
    * fix: hide scroll to top button when at top of page
    
    * fix: hr is now showing up
    
    * fix: adjust blog post padding for consistency
    
    * fix(css): colors
    
    * feat: add minute read time
    
    * fix: adjust date font sizing for consistency
    
    * feat: add styling to blockquote
    
    * feat: add styling to blog link
    resolve conflicting styling to button
    
    * feat: add mdx support
    
    * feat: fix image styling
    
    * fix: set images to eager loading
    
    * feat: disallow GPTBot
    
    * refactor: add draft property to blog posts
    
    * feat: add blog post how i became a web developer
    
    * feat(blog): remove LICENSE file
    
    * fix(blog): remove placeholder blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import { remarkReadingTime } from "./markdown-utils.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   vite: {
     ssr: {
       noExternal: ["react-icons"],
