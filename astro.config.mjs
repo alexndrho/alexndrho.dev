fd176d52e3e67e99d301c7c6b38fc97610c60b6f
Alexander Gabriel Ho <ho.alexander.g@gmail.com>
2025-12-14 11:31:13 +0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from "./markdown-utils.mjs";
@@ -9,7 +10,7 @@ const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
